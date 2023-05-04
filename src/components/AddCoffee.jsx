cadbba9c219f2382a2dd40a60f1b8a7732343923
Programming Hero <programming.hero1@gmail.com>
2023-05-04 09:33:18 -0500

    coffee input group

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -3,6 +3,27 @@ const AddCoffee = () => {
     return (
         <div>
             <h2>Add a Coffee</h2>
+            <form>
+                <div className="flex">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Coffee Name</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" placeholder="Coffee Name" className="input input-bordered" />
+                        </label>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Available Quantity</span>
+                        </label>
+                        <label className="input-group">
+                            <input type="text" placeholder="Available Quantity" className="input input-bordered" />
+                        </label>
+                    </div>
+                </div>
+
+            </form>
         </div>
     );
 };
