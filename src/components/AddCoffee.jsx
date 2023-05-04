d6ee60bfd920ce963117a7d48b134bd4cda69ed9
Programming Hero <programming.hero1@gmail.com>
2023-05-04 11:38:29 -0500

    send coffee data to the server and show success

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,3 +1,4 @@
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
     const handleAddCoffee = event => {
@@ -12,9 +13,31 @@ const AddCoffee = () => {
         const category = form.category.value;
         const details = form.details.value;
         const photo = form.photo.value;
-        
-        const newCoffee = {name, quantity, supplier, taste, category, details, photo}
+
+        const newCoffee = { name, quantity, supplier, taste, category, details, photo }
+
         console.log(newCoffee);
+
+        // send data to the server
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.insertedId){
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'User Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                      })
+                }
+            })
     }
 
 
