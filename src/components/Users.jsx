28d14fc6451de989e5c4b17124211fd7b574f10c
Programming Hero <programming.hero1@gmail.com>
2023-10-13 20:25:08 -0500

    delete user from client side

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 
 const Users = () => {
     const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
+    const handleDelete = id => {
+        // make sure user is confirmed to delete
+        fetch(`http://localhost:5000/user/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    console.log('deleted successfully');
+                    // remove the user from the UI
+                    const remainingUsers = users.filter(user => user._id !== id);
+                    setUsers(remainingUsers);
+                }
+            })
+    }
+
     return (
         <div>
             <h2>Users: {loadedUsers.length}</h2>
@@ -14,17 +33,21 @@ const Users = () => {
                             <th></th>
                             <th>Email</th>
                             <th>Created At</th>
+                            <th>Last Logged In</th>
                             <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
-                        {/* row 1 */}
                         {
-                            loadedUsers.map(user => <tr key={user._id}>
+                            users.map(user => <tr key={user._id}>
                                 <th>1</th>
                                 <td>{user.email}</td>
                                 <td>{user.createdAt}</td>
-                                <td>Blue</td>
+                                <td></td>
+                                <td>
+                                    <button
+                                        onClick={() => handleDelete(user._id)} className="btn">X</button>
+                                </td>
                             </tr>)
                         }
 
