fe10c2bc06ecd2587c73b0da5715cd4707b9fcae
Programming Hero <programming.hero1@gmail.com>
2023-10-17 22:22:35 -0500

    install tan stack query

---
+++ src/components/Users2.jsx
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+
+
+const Users2 = () => {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/user')
+            .then(res => res.json())
+            .then(data => {
+                setUsers(data)
+            })
+
+    }, [])
+
+
+
+
+    const handleDelete = id => {
+        // make sure user is confirmed to delete
+        fetch(`http://localhost:5000/user/${id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount > 0) {
+                    console.log('deleted successfully');
+                    // remove the user from the UI
+                    // const remainingUsers = users.filter(user => user._id !== id);
+                    // setUsers(remainingUsers);
+                }
+            })
+    }
+
+
+    return (
+        <div>
+            {/* <h2>Users: {loadedUsers.length}</h2> */}
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Email</th>
+                            <th>Created At</th>
+                            <th>Last Logged In</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map(user => <tr key={user._id}>
+                                <th>1</th>
+                                <td>{user.email}</td>
+                                <td>{user.createdAt}</td>
+                                <td>{user.lastLoggedAt}</td>
+                                <td>
+                                    <button
+                                        onClick={() => handleDelete(user._id)} className="btn">X</button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Users2;
--- src/main.jsx
+++ src/main.jsx
@@ -12,6 +12,11 @@ import SignUp from './components/SignUp.jsx';
 import SignIn from './components/SignIn.jsx';
 import AuthProvider from './providers/AuthProvider.jsx';
 import Users from './components/Users.jsx';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import Users2 from './components/Users2.jsx';
 
 const router = createBrowserRouter([
   {
@@ -34,18 +39,27 @@ const router = createBrowserRouter([
   {
     path: '/signin',
     element: <SignIn></SignIn>
-  }, 
+  },
   {
     path: '/users',
     element: <Users></Users>,
     loader: () => fetch('http://localhost:5000/user')
-  }
+  }, 
+  {
+    path: '/users2',
+    element: <Users2></Users2>,
+  }, 
 ]);
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </QueryClientProvider>
+
   </React.StrictMode>,
 )
